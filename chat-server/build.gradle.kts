b455e40053a61af99eb17347358d550e3a85cd60
성찬우 <166347412+hellochanoo@users.noreply.github.com>
2025-02-27 09:24:10 +0900

    README.md update

--- chat-server/build.gradle.kts
+++ chat-server/build.gradle.kts
@@ -22,16 +22,11 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-rsocket")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-
-    implementation("com.github.f4b6a3:uuid-creator:6.0.0")
-    implementation("org.postgresql:r2dbc-postgresql:1.0.7.RELEASE")
-
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.postgresql:postgresql")
@@ -51,16 +46,4 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> {
-    mainClass.set("ai.murple.api.MurfyApiApplicationKt")
-}
-
-tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootBuildImage> {
-    imageName.set("murfy-api:latest")
-}
-
-tasks.withType<Test> {
-    jvmArgs("-XX:+EnableDynamicAgentLoading")
 }
